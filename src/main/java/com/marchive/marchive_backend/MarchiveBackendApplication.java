b0e2c7335bbc7deb4da85049a09dc1648017929c
nourzoo <tmddus4671@gmail.com>
2026-07-13 17:21:21 +0900

    feat: Spring Boot 프로젝트 초기 구조 생성

---
+++ src/main/java/com/marchive/marchive_backend/MarchiveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.marchive.marchive_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarchiveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarchiveBackendApplication.class, args);
+	}
+
+}
