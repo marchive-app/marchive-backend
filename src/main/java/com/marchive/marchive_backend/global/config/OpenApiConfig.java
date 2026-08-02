5da70a908ae0218ca48747e9359854df7080fa09
nourzoo <tmddus4671@gmail.com>
2026-08-02 15:15:46 +0900

    feat: Swagger JWT 인증 설정 추가

--- src/main/java/com/marchive/marchive_backend/global/config/OpenApiConfig.java
+++ src/main/java/com/marchive/marchive_backend/global/config/OpenApiConfig.java
@@ -1,7 +1,12 @@
 package com.marchive.marchive_backend.global.config;
 
+import static org.apache.tomcat.websocket.BasicAuthenticator.schemeName;
+
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +19,13 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("Marchive API")
                         .version("1.0.0")
-                        .description("인스타그램 북마크 검색 마카이브 API"));
+                        .description("인스타그램 북마크 검색 마카이브 API"))
+                .addSecurityItem(new SecurityRequirement().addList(schemeName))
+                .components(new Components()
+                        .addSecuritySchemes(schemeName, new SecurityScheme()
+                                .name(schemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
