48b4796dffaa98a6487b8fe018cab5aa34c7da58
포라 <tmddus4671@gmail.com>
2026-08-02 16:02:53 +0900

    fix: 가짜 북마크 추가 (#16)

--- src/main/java/com/marchive/marchive_backend/chat/service/ChatMapper.java
+++ src/main/java/com/marchive/marchive_backend/chat/service/ChatMapper.java
@@ -11,15 +11,38 @@ import org.springframework.stereotype.Component;
 public class ChatMapper {
 
     public MessageDto toMessageDto(Message message) {
-        List<BookmarkDto> bookmarks = message.getBookmarks().stream()
-                .map(this::toBookmarkDto)
-                .toList();
+//        List<BookmarkDto> bookmarks = message.getBookmarks().stream()
+//                .map(this::toBookmarkDto)
+//                .toList();
+        List<BookmarkDto> bookmarks;
 
+        if (message.getRole() == Message.Role.assistant) {
+            // UI 확인용 mock 북마크 3개
+            bookmarks = mockBookmarks();
+        } else {
+            // user 메시지는 북마크 없음
+            bookmarks = List.of();
+        }
         return new MessageDto(
                 message.getMessageId(),
                 message.getRole().name(),
                 message.getContents(),
                 bookmarks
+        );
+    }
+
+    // UI 확인용 가짜 북마크 3개
+    private List<BookmarkDto> mockBookmarks() {
+        return List.of(
+                new BookmarkDto(1L,
+                        "https://picsum.photos/300?random=1",
+                        "https://instagram.com/p/DbZHxCVk-LS"),
+                new BookmarkDto(2L,
+                        "https://picsum.photos/300?random=2",
+                        "https://instagram.com/p/mockcode2"),
+                new BookmarkDto(3L,
+                        "https://picsum.photos/300?random=3",
+                        "https://instagram.com/p/mockcode3")
         );
     }
 
